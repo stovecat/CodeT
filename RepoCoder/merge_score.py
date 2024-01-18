e9d066e7a9ba43f4dd10603ee2fab962bf12472d
stovecat <stovecat@github.com>
2024-01-18 03:49:44 +0000

    display new EM ES scores by merge_score.py

--- RepoCoder/merge_score.py
+++ RepoCoder/merge_score.py
@@ -15,9 +15,16 @@ def get_n_chunk(base_path, benchmark, model, option):
 
 
 def get_scores(base_path="results", 
-               benchmarks=["random_api", "random_line"],
+               benchmarks=["random_line", "random_api"],
                model="codegen-2B-mono",
-               options=["none", "rg", "repocoder", "extractive_summary", "gt", "oracle"]):
+               options=["none", "rg", "repocoder", 
+#                         "extractive_summary",
+                        "extractive_summary_identifier", 
+                        "extractive_summary_entity",
+                        "extractive_summary_non_entity",
+                        "extractive_summary_non_entity_no_built_in_ids",
+                        "gt", "oracle"],
+               new_score=False):
     result_dict = {}
     for benchmark in benchmarks:
         result_dict[benchmark] = {}
@@ -33,16 +40,34 @@ def get_scores(base_path="results",
                     for key, val in tmp_result.items():
                         result_dict[benchmark][option][key].extend(val)    
                         
-    print(f"{model}")
-    for benchmark in benchmarks:
-        print(f"[{benchmark}]")
-        print("Option\tEM\tES")
-        for option in options:
-            if result_dict[benchmark][option] is None:
-                print(f"{option}\t-\t-")
-                continue
-            print(f"{option}\t{np.mean(result_dict[benchmark][option]['EM_scores'])}\t{np.mean(result_dict[benchmark][option]['ES_scores'])}")
-        print()
+    def display(new_score=False):                        
+        print(f"{model}")
+        for benchmark in benchmarks:
+            print(f"[{benchmark}]")
+            print("Option\tEM\tES")
+            for option in options:
+                if result_dict[benchmark][option] is None:
+                    print(f"{option}\t-\t-")
+                    continue
+                if new_score:
+#                     from compute_score import compute_new_EM, compute_new_ES
+                    new_EM_scores = []
+                    new_ES_scores = []
+                    for i in range(len(result_dict[benchmark][option]['EM_scores'])):
+#                         print(result_dict[benchmark][option].keys())
+                        target = result_dict[benchmark][option]['ground_truths'][i]
+                        preds = result_dict[benchmark][option]['predictions'][i]
+                        new_EM_scores.append(compute_new_EM(target=target, predictions=preds, passk=1))
+                        new_ES_scores.append(compute_new_ES(target=target, predictions=preds, passk=1))
+                    print(f"{option}\t{np.mean(new_EM_scores)}\t{np.mean(new_ES_scores)}")
+                    result_dict[benchmark][option]['new_EM_scores'] = new_EM_scores
+                    result_dict[benchmark][option]['new_ES_scores'] = new_ES_scores
+                else:
+                    print(f"{option}\t{np.mean(result_dict[benchmark][option]['EM_scores'])}\t{np.mean(result_dict[benchmark][option]['ES_scores'])}")
+            print()
+            
+    display(new_score)
+    
     return result_dict
 
 
